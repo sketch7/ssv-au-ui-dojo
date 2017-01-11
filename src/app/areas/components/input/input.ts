b0e90e1cdb64e280d899cdcd8c8cf9f939ffc09e
Stephen Lautier <stephen.lautier@outlook.com>
2017-01-11 02:16:17 +0100

    updated input sample

--- src/app/areas/components/input/input.ts
+++ src/app/areas/components/input/input.ts
@@ -3,4 +3,10 @@ import { autoinject } from "aurelia-framework";
 @autoinject
 export class InputSampleController {
 
+	isDisabled = true;
+
+	toggleDisable() {
+		this.isDisabled = !this.isDisabled;
+	}
+
 }
