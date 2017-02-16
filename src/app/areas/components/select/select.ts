1ada0ce360ea3e0cedc2cecb4bdc69b854209aa4
Clayton Lautier <clayton.lautier@outlook.com>
2017-02-16 02:53:54 +0100

    select: added onChange event

--- src/app/areas/components/select/select.ts
+++ src/app/areas/components/select/select.ts
@@ -1,4 +1,5 @@
 import { autoinject } from "aurelia-framework";
+import { ILog, LoggerFactory } from "@ssv/au-core";
 import { supportedSelectTypes, SelectItem } from "@ssv/au-ui";
 
 @autoinject
@@ -6,11 +7,26 @@ export class SelectSampleController {
 
 	types: string[] = supportedSelectTypes;
 	colors = [null, "primary", "accent", "danger", "warn", "success", "info"];
-
 	options: SelectItem[] = [
 		{ text: "League of Legends", value: "lol" },
 		{ text: "World Of Warcraft", value: "wow" },
 		{ text: "Heroes of the Storm", value: "hots" }
 	];
+
+	private logger: ILog;
+
+	constructor(
+		loggerFactory: LoggerFactory
+	) {
+		this.logger = loggerFactory.get("selectSampleController");
+	}
+
+	onChange(event: CustomEvent) {
+		this.logger.debug("onChange", "triggered >>>", {
+			previous: event.detail.previous,
+			value: event.detail.value,
+			event: event
+		});
+	}
 
 }
