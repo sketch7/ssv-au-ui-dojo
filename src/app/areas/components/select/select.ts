d23f581e883d97b4b66d6695e71ee322956a75f4
Clayton Lautier <clayton.lautier@outlook.com>
2017-03-01 02:54:59 +0100

    select: updated examples with multi select, initial set selected values.

--- src/app/areas/components/select/select.ts
+++ src/app/areas/components/select/select.ts
@@ -1,16 +1,31 @@
 import { autoinject } from "aurelia-framework";
 import { ILog, LoggerFactory } from "@ssv/au-core";
-import { supportedSelectTypes, SelectItem } from "@ssv/au-ui";
+import { supportedSelectTypes } from "@ssv/au-ui";
 
 @autoinject
 export class SelectSampleController {
 
 	types: string[] = supportedSelectTypes;
 	colors = [null, "primary", "accent", "danger", "warn", "success", "info"];
-	options: SelectItem[] = [
+	options: any[] = [
 		{ text: "League of Legends", value: "lol" },
 		{ text: "World Of Warcraft", value: "wow" },
 		{ text: "Heroes of the Storm", value: "hots" }
+	];
+	optionSelected = null;
+	optionsSelected: any[] = [
+		{ text: "World Of Warcraft", value: "wow" }
+	];
+
+	heroes: any[] = [
+		{ id: 1, name: "thor", strength: 20 },
+		{ id: 2, name: "iron man", strength: 11 },
+		{ id: 3, name: "batman", strength: 7 }
+	];
+
+	heroSelected = { id: 2, name: "iron man", strength: 11 };
+	heroesSelected: any[] = [
+		{ id: 3, name: "batman", strength: 7 }
 	];
 
 	private logger: ILog;
