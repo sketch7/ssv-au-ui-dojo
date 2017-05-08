d31bf86a45e6980cc0199848f99201ddf5825b57
Clayton Lautier <clayton.lautier@outlook.com>
2017-05-09 01:55:43 +0200

    chips: update example to reflect au-ui changes

--- src/app/areas/components/chips/chips.ts
+++ src/app/areas/components/chips/chips.ts
@@ -1,6 +1,6 @@
 import { autoinject } from "aurelia-framework";
 import { ILog, LoggerFactory } from "@ssv/au-core";
-import { supportedChipTypes } from "@ssv/au-ui";
+import { supportedChipTypes, supportedFillStyles } from "@ssv/au-ui";
 
 @autoinject
 export class ChipsSampleController {
@@ -10,14 +10,16 @@ export class ChipsSampleController {
 	colors = [null, "primary", "accent", "danger", "warn", "success", "info"];
 
 	fruits = ["banana", "strawberry", "melon"];
+	fillStyle = supportedFillStyles;
+	focusStyle = supportedFillStyles;
 
 	options: any[] = [
-		{ text: "Barbarian", value: "barbarian", imgSrc: "/dist/assets/imgs/barb_150x190.jpg" },
-		{ text: "Monk", value: "monk", imgText: "MNKI" },
-		{ text: "Crusader", value: "crusader", imgIcon: "shield", allowRemove: false },
+		{ text: "Barbarian", value: "barbarian", iconImage: "/dist/assets/imgs/barb_150x190.jpg" },
+		{ text: "Monk", value: "monk", iconText: "MNKI" },
+		{ text: "Crusader", value: "crusader", iconName: "shield", allowRemove: false },
 		{ text: "Demon Hunter", value: "demon-hunter", allowRemove: false },
 		{ text: "Wizard", value: "wizard", allowRemove: true }
-		// { text: "Demon Hunter", value: "demon-hunter", allowRemove: false, imgSrc: "/dist/assets/imgs/yuna.jpg" }
+		// { text: "Demon Hunter", value: "demon-hunter", allowRemove: false, iconImage: "/dist/assets/imgs/yuna.jpg" }
 	];
 
 	availableOptions: any[] = [
@@ -40,7 +42,7 @@ export class ChipsSampleController {
 	onRemove(event: CustomEvent) {
 		this.logger.debug("onRemove", "item has been removed >>>", {
 			value: event.detail.value,
-			event: event
+			event
 		});
 	}
 
